4e23605771ab99e3a57455563caac9b7a5cf9a78
[sureshy0272] <[sureshyemmani1234@gmail.com]>
2023-04-29 22:27:34 +0530

    repo

---
+++ index.js
@@ -0,0 +1,28 @@
+function getMenu() {
+    
+}
+
+
+function TakeOrder() {
+    setTimeout(function() {
+
+    },2500)
+}
+
+
+function orderPrep() {
+    setTimeout(function() {
+
+    },1500)
+}
+
+function orderPay() {
+    setTimeout(function() {
+
+    },1000)
+
+}
+
+function thankyouFnc() {
+    alert("Thank you for eating with us!!")
+}
